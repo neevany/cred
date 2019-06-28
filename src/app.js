624d3d0c229d2734664588dc03c47fa3e1d4a9c2
neevany <yannamani.naveen@gmail.com>
2019-06-28 10:25:06 +0530

    initial react setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import style from './app.less';
+
+class AppComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div className="cred-container">
+        Cred Calendar
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComponent from './app';
+
+ReactDOM.render(<AppComponent />, document.getElementById('app'));
+
+module.hot.accept();
